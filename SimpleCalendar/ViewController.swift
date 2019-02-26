8601d68910219c8707fb4e06f7534fc6e3172596
Ilia Kolomeitsev <ilya.kolo@gmail.com>
2019-02-26 20:41:29 +0200

    Initial Commit

---
+++ SimpleCalendar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleCalendar
+//
+//  Created by Ilia Kolomeitsev on 26/02/2019.
+//  Copyright © 2019 Ilia Kolomeitsev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
